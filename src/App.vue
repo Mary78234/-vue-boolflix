3586125ffcfa65841ab6e0285a088ef5c4f20466
Mary78234 <wang11quanquan@gmail.com>
2021-05-26 19:52:07 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <body>
+    <div id="app">
+      
+      <Header />
+      <Main />
+
+    </div>
+  </body>
+</template>
+
+<script>
+import axios from 'axios';
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main
+  },
+  data(){
+    return{
+      axios,
+      apiURL: 'https://api.themoviedb.org/3/search/movie',
+      apiKey: 'c2ea226cb0288d7001760b50ed3a2e3f',
+      query: 'lupen',
+      film: []
+    }
+  },
+  created(){
+    axios.get(this.apiURL,{
+        params:{
+          api_key: this.apiKey,
+          query: this.query,
+          language: 'it-IT'
+        }
+      })
+      .then(resp =>{
+        console.log(resp);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }/* ,
+  methods:{
+    searchFilm(){
+      axios.get(this.apiURL,{
+        params:{
+          api_key: this.apiKey,
+          query: this.query,
+          language: 'it-IT'
+        }
+      })
+      .then(resp =>{
+        console.log(resp);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+
+  } */
+
+}
+</script>
+
+<style lang="scss">
+@import 'assets/style/general.scss';
+@import 'assets/style/utilities.scss';
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <h1>Lista film</h1>
+    <input type="text" placeholder="Inserire Film da cercare">
+    <button>Cerca</button>
+
+    <ul>
+      <li></li>
+    </ul>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props:{
+    film: Array
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
